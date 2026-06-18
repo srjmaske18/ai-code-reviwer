85bb11626eb8c7a601667eb873ca5a508d612935
Shubham Maske <srjmaske18@gmail.com>
2026-06-18 11:00:49 +0530

    github sign in pade designing under construction

---
+++ lib/auth.ts
@@ -0,0 +1,17 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { prisma } from "./db";
+import { nextCookies } from "better-auth/next-js";
+
+export const auth = betterAuth({
+   database: prismaAdapter(prisma, {
+        provider: "postgresql", // or "mysql", "postgresql", ...etc
+    }),
+    socialProviders: { 
+    github: { 
+      clientId: process.env.GITHUB_CLIENT_ID as string, 
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string, 
+    }, 
+  }, 
+  plugins: [nextCookies()]
+});
